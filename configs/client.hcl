58bcdc782f3b497008f4ef35c3a9945b0cd3442d
Ben <ben@ashtonresilience.com>
2023-01-08 20:07:17 +0000

    Now correctly sets up NoMad service

--- configs/client.hcl
+++ configs/client.hcl
@@ -1,5 +1,6 @@
 datacenter = "local"
 data_dir   = "/etc/nomad.d"
+log_file = "C:/nomad/log/nomad.log"
 
 client {
   enabled = true
--- setupNomad.ps1
+++ setupNomad.ps1
@@ -28,14 +28,22 @@ if (($option1 -eq "server") -or ($option1 -eq "client")) {
 }
 
 New-Item -Path "C:\nomad\config" -ItemType directory
+New-Item -Path "C:\nomad\data" -ItemType directory
+New-Item -Path "C:\nomad\log" -ItemType directory
+
+
 if ($option1 -eq "server") {
     Copy-Item "$scriptPath\configs\server.hcl" -Destination "$basePath\config\server.hcl"
-    New-Service -Name "Nomad" -BinaryPathName "$exePath agent -config=$basePath\config\server.hcl" -StartupType Automatic
+    New-Service -Name "Nomad" -BinaryPathName "$exePath agent -config=$basePath\config\server.hcl -data-dir=C:\nomad\data" -StartupType Automatic
+    New-NetFirewallRule -DisplayName "NOMAD Serf WAN TCP" -Direction inbound -Profile Any -Action Allow -LocalPort 4648 -Protocol TCP
+    New-NetFirewallRule -DisplayName "NOMAD Serf WAN UDP" -Direction inbound -Profile Any -Action Allow -LocalPort 4648 -Protocol UDP
 } elseif ($option1 -eq "client") {
     Copy-Item "$scriptPath\configs\client.hcl" -Destination "$basePath\config\client.hcl"
-    New-Service -Name "Nomad" -BinaryPathName "$exePath agent -config=$basePath\config\client.hcl" -StartupType Automatic
+    New-Service -Name "Nomad" -BinaryPathName "$exePath agent -config=$basePath\config\client.hcl -data-dir=C:\nomad\data" -StartupType Automatic
 }
 
 Start-Service -Name "Nomad" -PassThru
 
-# create logging via config (.hcl) files and create logs folder as part of installation
+# For both NoMad Servers and Clients
+New-NetFirewallRule -DisplayName "NOMAD HTTP API" -Direction inbound -Profile Any -Action Allow -LocalPort 4646 -Protocol TCP
+New-NetFirewallRule -DisplayName "NOMAD RPC" -Direction inbound -Profile Any -Action Allow -LocalPort 4647 -Protocol TCP
